e9dc530cdc1bcad8d3e5fb3fcd9c6dad57fb7a21
Harshal Gondaliya <159172017+harshalgondaliya@users.noreply.github.com>
2024-03-03 09:55:40 +0530

    Add files via upload

---
+++ Portfolio/main/Project02/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/roll_dice')
+def roll_dice():
+    dice1_number = random.randint(1, 6)
+    dice2_number = random.randint(1, 6)
+    return render_template('index.html', dice1=dice1_number, dice2=dice2_number)
+
+if __name__ == '__main__':
+    app.run(debug=True)
